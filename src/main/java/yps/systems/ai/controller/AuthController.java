d57e2f554db4a2109e6f8f91283ace95b1e59c5d
Aldve2 <jakile971012@outlook.com>
2025-07-25 11:33:18 -0500

    Update AuthController.java

--- src/main/java/yps/systems/ai/controller/AuthController.java
+++ src/main/java/yps/systems/ai/controller/AuthController.java
@@ -1,5 +1,7 @@
 package yps.systems.ai.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
